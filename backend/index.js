7cd8e546e1f0105ab7f876d98a0a6515647820ed
landenenglish <englishlanden@gmail.com>
2022-07-09 14:22:49 -0400

    completed sign up and log in

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
-const app = express();
+const app = express()
 
 // Express Settings
 app.use(cors())
@@ -17,8 +17,9 @@ app.use(express.urlencoded({ extended: true }))
 
 app.use('/places', require('./controllers/places'))
 app.use('/users', require('./controllers/users'))
+app.use('/authentication', require('./controllers/authentication'))
 
 // Listen for Connections
 app.listen(process.env.PORT, () => {
-    console.log(`Listening on ${process.env.PORT}`)
+  console.log(`Listening on ${process.env.PORT}`)
 })
